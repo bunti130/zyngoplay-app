127b01deebcf4496c62c5b67ca9bfbe7575324f3
bunti130 <37932682+bunti130@users.noreply.github.com>
2026-03-13 15:34:23 +0530

    Update app.js

--- www/app.js
+++ www/app.js
@@ -1,6 +1,7 @@
 const SERVER_URL = "https://zyngoplay-server.onrender.com";
 
 let socket = null;
+let userId = "user_" + Math.floor(Math.random()*100000);
 
 function connectSocket(){
 
@@ -18,10 +19,43 @@ socket.on("match_found", function(roomId){
 
 console.log("Match found:", roomId);
 
-alert("Match Found! Room: " + roomId);
+alert("Match Found!");
+
+joinRoom(roomId);
+
+});
+
+socket.on("players", function(players){
+
+console.log("Players in room:", players);
 
 });
 
 }
 
+function findMatch(game){
+
+if(!socket){
 connectSocket();
+}
+
+console.log("Searching match for:", game);
+
+socket.emit("find_match",{
+userId:userId,
+game:game,
+entryFee:10
+});
+
+}
+
+function joinRoom(roomId){
+
+socket.emit("join_room",{
+roomId:roomId,
+userId:userId
+});
+
+}
+
+connectSocket();l
