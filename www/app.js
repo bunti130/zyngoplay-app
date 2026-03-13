2007337fdf2b644b4bdfa8fca05998dadd5439e7
bunti130 <37932682+bunti130@users.noreply.github.com>
2026-03-13 16:34:16 +0530

    Update app.js

--- www/app.js
+++ www/app.js
@@ -2,6 +2,7 @@ const SERVER_URL = "https://zyngoplay-server.onrender.com";
 
 let socket;
 let userId = "user_" + Math.floor(Math.random()*100000);
+let currentRoom = null;
 
 function connectSocket(){
 
@@ -15,21 +16,24 @@ console.log("Connected:", socket.id);
 
 socket.on("match_found", (roomId) => {
 
-console.log("Match found:", roomId);
+currentRoom = roomId;
 
 joinRoom(roomId);
-  
+
 window.location = "room.html";
+
 });
 
 socket.on("players", (players) => {
 
-console.log("Players in room:", players);
+console.log("Players:", players);
 
-if(players.length >= 2){
+});
 
-alert("Game Starting!");
+socket.on("ludo_update", (data) => {
 
+if(typeof updateBoard === "function"){
+updateBoard(data);
 }
 
 });
@@ -38,11 +42,11 @@ alert("Game Starting!");
 
 function findMatch(game){
 
-socket.emit("find_match", {
+socket.emit("find_match",{
 
-userId: userId,
-game: game,
-entryFee: 10
+userId:userId,
+game:game,
+entryFee:10
 
 });
 
@@ -50,10 +54,22 @@ entryFee: 10
 
 function joinRoom(roomId){
 
-socket.emit("join_room", {
+socket.emit("join_room",{
 
-roomId: roomId,
-userId: userId
+roomId:roomId,
+userId:userId
+
+});
+
+}
+
+function sendMove(steps){
+
+socket.emit("ludo_move",{
+
+roomId:currentRoom,
+player:userId,
+steps:steps
 
 });
 
