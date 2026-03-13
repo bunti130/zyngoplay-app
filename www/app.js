4194d9bd7818162ec97be1b2204fcc7a55484773
bunti130 <37932682+bunti130@users.noreply.github.com>
2026-03-13 18:21:44 +0530

    Update app.js

--- www/app.js
+++ www/app.js
@@ -4,6 +4,8 @@ let socket;
 let userId = "user_" + Math.floor(Math.random()*100000);
 let currentRoom = null;
 
+/* CONNECT SOCKET */
+
 function connectSocket(){
 
 socket = io(SERVER_URL);
@@ -14,21 +16,47 @@ console.log("Connected:", socket.id);
 
 });
 
+/* MATCH FOUND */
+
 socket.on("match_found", (roomId) => {
 
 currentRoom = roomId;
 
 joinRoom(roomId);
 
-window.location = "room.html";
+/* detect game */
+
+const game = localStorage.getItem("selectedGame");
+
+/* redirect to game */
+
+if(game === "ludo"){
+window.location = "ludo.html";
+}
+
+if(game === "poker"){
+window.location = "poker.html";
+}
+
+if(game === "rummy"){
+window.location = "rummy.html";
+}
+
+if(game === "carrom"){
+window.location = "carrom.html";
+}
 
 });
+
+/* ROOM PLAYERS */
 
 socket.on("players", (players) => {
 
 console.log("Players:", players);
 
 });
+
+/* LUDO UPDATE */
 
 socket.on("ludo_update", (data) => {
 
@@ -38,7 +66,39 @@ updateBoard(data);
 
 });
 
+/* POKER UPDATE */
+
+socket.on("poker_update", (data) => {
+
+if(typeof updatePoker === "function"){
+updatePoker(data);
 }
+
+});
+
+/* RUMMY UPDATE */
+
+socket.on("rummy_update", (data) => {
+
+if(typeof updateRummy === "function"){
+updateRummy(data);
+}
+
+});
+
+/* CARROM UPDATE */
+
+socket.on("carrom_update", (data) => {
+
+if(typeof updateCarrom === "function"){
+updateCarrom(data);
+}
+
+});
+
+}
+
+/* FIND MATCH */
 
 function findMatch(game){
 
@@ -52,6 +112,18 @@ entryFee:10
 
 }
 
+/* START GAME */
+
+function startGame(game){
+
+localStorage.setItem("selectedGame",game);
+
+findMatch(game);
+
+}
+
+/* JOIN ROOM */
+
 function joinRoom(roomId){
 
 socket.emit("join_room",{
@@ -62,6 +134,8 @@ userId:userId
 });
 
 }
+
+/* LUDO MOVE */
 
 function sendMove(steps){
 
@@ -74,5 +148,61 @@ steps:steps
 });
 
 }
+
+/* POKER ACTION */
+
+function pokerAction(action){
+
+socket.emit("poker_action",{
+
+roomId:currentRoom,
+player:userId,
+action:action
+
+});
+
+}
+
+/* RUMMY DRAW */
+
+function rummyDraw(){
+
+socket.emit("rummy_draw",{
+
+roomId:currentRoom,
+player:userId
+
+});
+
+}
+
+/* RUMMY DISCARD */
+
+function rummyDiscard(card){
+
+socket.emit("rummy_discard",{
+
+roomId:currentRoom,
+player:userId,
+card:card
+
+});
+
+}
+
+/* CARROM POT */
+
+function carromPot(){
+
+socket.emit("carrom_pot",{
+
+roomId:currentRoom,
+player:userId
+
+});
+
+}
+
+/* START CONNECTION */
 
 connectSocket();
